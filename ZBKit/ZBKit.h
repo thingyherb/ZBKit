4292e058cef62e0f5885e6d82d53a85f8575a4f4
zhaomy <zhaomy0000@gmail.com>
2015-07-23 10:12:44 +0800

    0.0.1

---
+++ ZBKit/ZBKit.h
@@ -0,0 +1,16 @@
+//
+//  ZBKit.h
+//  ZBKitDemo
+//
+//  Created by ZhaoMY on 7/23/15.
+//  Copyright (c) 2015 Zhao. All rights reserved.
+//
+
+#ifndef ZBKitDemo_ZBKit_h
+#define ZBKitDemo_ZBKit_h
+
+#import "UIView+ZBAutoLayout.h"
+
+
+
+#endif
---
+++ ZBKit/ZBLayout/UIView+ZBAutoLayout.m
@@ -0,0 +1,55 @@
+//
+//  UIView+ZBAutoLayout.m
+//  ZBKitDemo
+//
+//  Created by ZhaoMY on 7/23/15.
+//  Copyright (c) 2015 Zhao. All rights reserved.
+//
+
+#import "UIView+ZBAutoLayout.h"
+
+@implementation UIView (ZBAutoLayout)
+
+
+
+#pragma mark - Getters & Setters
+
+static Constrain _constrain;
+
+- (Constrain)constrain {
+    
+    return _constrain;
+}
+
+- (void)setConstrain:(Constrain)constrain {
+    
+    _constrain = constrain;
+    
+    if (!self.superview) {
+        
+        NSLog(@"Error: %@ must have a superview",self);
+        
+        return;
+    }
+    
+    self.translatesAutoresizingMaskIntoConstraints = false;
+    NSArray *hConstrains = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-x-[self(==width)]"
+                                                                   options:0
+                                                                   metrics:@{@"x":@(_constrain.x),@"width":@(_constrain.width)}
+                                                                     views:NSDictionaryOfVariableBindings(self)];
+    NSArray *vConstrains = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-y-[self(==height)]"
+                                                                   options:0
+                                                                   metrics:@{@"y":@(_constrain.y),@"height":@(_constrain.height)}
+                                                                     views:NSDictionaryOfVariableBindings(self)];
+    [self.superview addConstraints:hConstrains];
+    [self.superview addConstraints:vConstrains];
+}
+
+@end
+
+
+
+
+
+
+
