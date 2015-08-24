c1291c4f3b518edb0a4cd1eb1ebcadcadca97342
zhaomy <zhaomy0000@gmail.com>
2015-08-24 18:25:39 +0800

    0.0.2

---
+++ ZBKit/ZBCheck/NSMutableArray+ZBCheck.m
@@ -0,0 +1,90 @@
+//
+//  NSMutableArray+ZBCheck.m
+//  ZBKitDemo
+//
+//  Created by ZhaoMY on 8/24/15.
+//  Copyright (c) 2015 Zhao. All rights reserved.
+//
+
+#import "NSMutableArray+ZBCheck.h"
+#import <objc/runtime.h>
+
+@implementation NSMutableArray (ZBCheck)
+
+
+static IMP _targetImp  = NULL;
+static IMP _targetImp2 = NULL;
+
+- (id)checkObjectAtIndex:(NSUInteger)index {
+    
+    [self restore];
+    
+    id result = nil;
+    
+    if (index < self.count) {
+        
+        result = self[index];
+        
+    } else {
+        
+        NSLog(@"数组越界：%@", self.description);
+    }
+    
+    return result;
+}
+
+- (void)checkInsertObject:(id)anObject atIndex:(NSUInteger)index {
+    
+    [self restore];
+    
+    if (anObject) {
+        
+        [self insertObject:anObject atIndex:index];
+        
+    } else {
+     
+        NSLog(@"插入的数据为空：%@",self);
+    }
+}
+
+- (void)restore {
+    
+    class_replaceMethod([self class], @selector(objectAtIndex:), _targetImp, NULL);
+    class_replaceMethod([self class], @selector(insertObject:atIndex:), _targetImp2, NULL);
+}
+
+- (NSArray *)check {
+    
+    if (!_targetImp) {
+        
+        _targetImp  = [self methodForSelector:@selector(objectAtIndex:)];
+    }
+    
+    if (!_targetImp2) {
+        
+        _targetImp2 = [self methodForSelector:@selector(insertObject:atIndex:)];
+    }
+    
+    IMP imp = [self methodForSelector:@selector(checkObjectAtIndex:)];
+    class_replaceMethod([self class], @selector(objectAtIndex:), imp, NULL);
+    
+    IMP imp2 = [self methodForSelector:@selector(checkInsertObject:atIndex:)];
+    class_replaceMethod([self class], @selector(insertObject:atIndex:), imp2, NULL);
+    
+    
+    
+    return self;
+}
+
+
+
+
+
+
+
+@end
+
+
+
+
+
---
+++ ZBKit/ZBCheck/NSMutableDictionary+ZBCheck.m
@@ -0,0 +1,49 @@
+//
+//  NSMutableDictionary+ZBCheck.m
+//  ZBKitDemo
+//
+//  Created by ZhaoMY on 8/24/15.
+//  Copyright (c) 2015 Zhao. All rights reserved.
+//
+
+#import "NSMutableDictionary+ZBCheck.h"
+#import <objc/runtime.h>
+
+@implementation NSMutableDictionary (ZBCheck)
+
+static IMP _targetImp = NULL;
+
+- (void)checkSetObject:(id)anObject forKey:(id <NSCopying>)aKey {
+    
+    [self restore];
+    
+    if (anObject) {
+        
+        [self setObject:anObject forKey:aKey];
+        
+    } else {
+        
+        NSLog(@"插入的数据为空：%@",self);
+    }
+}
+
+- (void)restore {
+    
+    class_replaceMethod([self class], @selector(setObject:forKey:), _targetImp, NULL);
+}
+
+- (NSMutableDictionary *)check {
+    
+    if (!_targetImp) {
+        
+        _targetImp = [self methodForSelector:@selector(setObject:forKey:)];
+    }
+    
+    IMP imp = [self methodForSelector:@selector(checkSetObject:forKey:)];
+    class_replaceMethod([self class], @selector(setObject:forKey:), imp, NULL);
+    
+    return self;
+}
+
+
+@end
